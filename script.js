67015180e6b0ef440a9592761ee13b065adc7825
Hoang <148619566+vnvhoang@users.noreply.github.com>
2023-10-28 22:19:37 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the current page's filename (e.g., "index.html" or "resume.html")
+    var currentFileName = location.pathname.split('/').slice(-1)[0];
+
+    // Add an "active" class to the sub-page link that corresponds to the current page
+    var subpageLink = document.querySelector(`a[href="${currentFileName}"]`);
+    if (subpageLink) {
+        subpageLink.classList.add("active");
+    }
+
+    // Array of background images
+    var backgroundImages = [
+        'Background1.jpg',
+        'Background2.jpg',
+        'Background3.jpg'
+        // Add more image URLs as needed
+    ];
+
+    var currentImageIndex = 0;
+    var body = document.body;
+
+    function changeBackgroundImage() {
+        body.style.backgroundImage = `url('${backgroundImages[currentImageIndex]}')`;
+        currentImageIndex = (currentImageIndex + 1) % backgroundImages.length;
+    }
+
+    // Change background image every 1 minute (60000 milliseconds)
+    setInterval(changeBackgroundImage, 60000);
+});
