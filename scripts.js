fb780c15b6692de07df6439c6ea880724eafb5d5
Hoang <148619566+vnvhoang@users.noreply.github.com>
2024-05-20 22:36:53 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,1 @@
-// scripts.js
 
-// Smooth scrolling for navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
